e7427957c1f53d85fdf87cc8c286aa073b89dc9c
Joel Van Horn <joel@beta.io>
2012-04-24 13:08:04 -0400

    added some debugging

--- init.rb
+++ init.rb
@@ -1,1 +1,6 @@
 require "config/heroku/command/config"
+
+if Heroku::VERSION >= "2.0"
+  Heroku::Command.global_option :filename, "--filename FILENAME"
+  Heroku::Command.global_option :appenv, "--appenv"
+end
