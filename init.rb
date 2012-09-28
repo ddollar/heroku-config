83a53f342542adb048970dd923208773668b381a
Nicholas Henry <nicholas.henry@gmail.com>
2012-09-28 18:09:54 -0400

    Upgrade to use the Heroku API
    * This eliminates the various 'DEPRECATED' notices caused by using the Heroku::Client with recent versions of the Heroku gem
    * Fixes issue #3

--- init.rb
+++ init.rb
@@ -1,1 +1,13 @@
 require "config/heroku/command/config"
+
+begin
+  require "heroku-api"
+rescue LoadError
+  puts <<-MSG
+  This plugin is not compatiable with the version of Heroku CLI installed. If
+  you have installed the Heroku gem, please upgrade with the following command:
+
+  gem update heroku
+  MSG
+  exit
+end
