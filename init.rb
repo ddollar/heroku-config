43ef4501788a6298ff84ca150491cba4c57c65ba
Joel Van Horn <joel@joelvanhorn.com>
2013-08-25 18:56:19 -0400

    removed env and option

--- init.rb
+++ init.rb
@@ -2,7 +2,7 @@ require "config/heroku/command/config"
 
 if Heroku::VERSION >= "2.0"
   # specify a specific filename to use
-  Heroku::Command.global_option :filename, "-f", "--filename FILENAME"
+  #Heroku::Command.global_option :filename, "-f", "--filename FILENAME"
 end
 
 begin
